4ac84e9898b63878084bc94ce7ca64a29985bcfc
mateusz <mateusz@stake.fish>
2023-02-19 13:26:02 -0300

    WIP

--- grid_trading/utils/print_strategy_table.py
+++ grid_trading/utils/print_strategy_table.py
@@ -19,15 +19,18 @@ def print_strategy_table(strategy_list: List[BaseStrategyDao]):
     table.add_column("Status", justify="right", style="green")
 
     for strategy in strategy_list:
-        Token0Class = get_token_class_by_name(strategy.token0)
         Token1Class = get_token_class_by_name(strategy.token1)
+
+        last_tx_price = (
+            to_human(strategy.last_tx_price, decimals=Token1Class.decimals()) if strategy.last_tx_price is not None else "-"
+        )
 
         table.add_row(
             str(strategy.id),
             strategy.token0,
             strategy.token1,
-            f"{to_human(strategy.reference_price, decimals=Token0Class.decimals())} {strategy.token0}",
-            f"{to_human(strategy.last_tx_price, decimals=Token1Class.decimals())} {strategy.token1}",
+            f"{to_human(strategy.reference_price, decimals=Token1Class.decimals())} {strategy.token1}",
+            f"{last_tx_price} {strategy.token1}",
             f"{int(strategy.grid_every_percent * 100)} %",
             f"{int(strategy.percent_per_flip * 100)} %",
             strategy.status.value,
